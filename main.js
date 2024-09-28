efa71a0baab12cb0b3b2ebebf5c063d9928176c9
amruta khatawate <amrutakhatawate206@gmail.com>
2024-09-28 20:37:38 +0530

    Added a functionality

--- main.js
+++ main.js
@@ -7,11 +7,18 @@ console.log(display.innerHTML);
 function getData(button){
       let value = button.innerHTML;
       result += value;
-      display.innerHTML = result;
+      // display.innerHTML = result;
+      displayResult();
       console.log(result); 
 }
 
 function popString(){
       result = result.slice(0, -1);
-      display.innerHTML = result;
+      displayResult();
+      // display.innerHTML = result;
+}
+
+function displayResult(){
+      if(result.length <= 20)
+            display.innerHTML = result;
 }
